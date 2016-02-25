4be2d71ab6041056ce40f367c0e60671370ed562
olix20 <md.oliya@gmail.com>
2016-02-25 23:43:06 +0100

    Added a function to feature_format.py to convert data in Pandas dataframe for quicker caluclations

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,8 +16,22 @@
 """
 
 import pickle
+import numpy
+import matplotlib.pyplot as plt
+import sys
+sys.path.append("../tools/")
+from feature_format import *
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+from scipy import stats
+import seaborn as sns
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+
+#REMOVING OUTLIER!!
+enron_data.pop("TOTAL", 0)
 
 count = 0
 salaryCount =0
@@ -26,7 +40,7 @@ totalPayments = 0
 
 for key in enron_data.keys():
     #print key
-    #print enron_data[key]
+    # print enron_data[key]
     if(enron_data[key]['poi']==True):
         count += 1
         # print enron_data[key]['email_address']
@@ -51,18 +65,66 @@ print "totalPayments NAN: " ,totalPayments
 # print "LAY : " , enron_data["LAY KENNETH L"]
 # print "FASTOW ANDREW S : " , enron_data["FASTOW ANDREW S"]
 
-import numpy as np
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
+def getMissingValueRatio(data):
+    """
+    data is a pandas dataframe
+    """
+    ratio = len(data[data=='NaN'])/len(data[0])
+    return ratio
 
 
-def plotFeature(feature, enron_data):
+feature_1 = "salary"
+feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 
-    for key in enron_data.keys():
-        #print key
-        #print enron_data[key]
-        if(enron_data[key]['poi']==True):
-            count += 1
+poi  = "poi"
+# features_list = [poi, feature_1, feature_2]
+features_list = enron_data["FASTOW ANDREW S"].keys()
+features_list.remove('email_address')
+features_list.remove('other')
+features_list.remove('poi')
+
+
+
+
+keys = ['salary', 'to_messages', 'deferral_payments',
+'total_payments', 'exercised_stock_options', 'bonus',
+'restricted_stock', 'shared_receipt_with_poi',
+'restricted_stock_deferred', 'total_stock_value', 'expenses',
+'loan_advances', 'from_messages', 'other', 'from_this_person_to_poi',
+'poi', 'director_fees', 'deferred_income', 'long_term_incentive',
+'email_address', 'from_poi_to_this_person']
+
+# print [poi]+features_list
+
+# data = featureFormat(enron_data, [poi]+features_list )
+# poi, finance_features = targetFeatureSplit( data )
+
+
+# # finance_features.insert(0,[200000.,1000000.])
+# print "before scaling: ", stats.describe(finance_features)
+
+edata = getFeaturesAsPandaDict(enron_data,features_list)
+# edata.to_csv(path_or_buf="./dataDump.csv")
+# print getMissingValueRatio(edata)
+
+# print getFeaturesAsPandaDict(enron_data,features_list).describe()
+# print edata['salary']
+N = len(edata['salary'])
+plt.figure()
+# edata['salary'].hist()
+fig, ax = plt.subplots()
+
+rects1 = ax.bar(np.arange(N), edata['salary'], 0.35, color='b')
+
+# plt.show()
+# def plotFeature(feature, enron_data):
+#
+#     for key in enron_data.keys():
+#         #print key
+#         #print enron_data[key]
+#         if(enron_data[key]['poi']==True):
+#             count += 1
 
 
 # try:
@@ -72,7 +134,7 @@ def plotFeature(feature, enron_data):
 # plt.scatter(ages, net_worths)
 # plt.show()
 #
-plotFeature("salary",enron_data)
+# plotFeature("salary",enron_data)
 
 
 
--- tools/feature_format.py
+++ tools/feature_format.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-""" 
+"""
     A general tool for converting data from the
-    dictionary format to an (n x k) python list that's 
+    dictionary format to an (n x k) python list that's
     ready for training an sklearn algorithm
 
     n--no. of key-value pairs in dictonary
@@ -13,7 +13,7 @@
         key-value pair in the dict is the name
         of a feature, and its value for that person
 
-    In addition to converting a dictionary to a numpy 
+    In addition to converting a dictionary to a numpy
     array, you may want to separate the labels from the
     features--this is what targetFeatureSplit is for
 
@@ -21,7 +21,7 @@
     and the features you want to use are the person's
     salary and bonus, here's what you would do:
 
-    feature_list = ["poi", "salary", "bonus"] 
+    feature_list = ["poi", "salary", "bonus"]
     data_array = featureFormat( data_dictionary, feature_list )
     label, features = targetFeatureSplit(data_array)
 
@@ -32,6 +32,7 @@
 
 
 import numpy as np
+import pandas as pd
 
 def featureFormat( dictionary, features, remove_NaN=True, remove_all_zeroes=True, remove_any_zeroes=False, sort_keys = False):
     """ convert dictionary to numpy array of features
@@ -103,15 +104,15 @@ def featureFormat( dictionary, features, remove_NaN=True, remove_all_zeroes=True
 
 
 def targetFeatureSplit( data ):
-    """ 
+    """
         given a numpy array like the one returned from
         featureFormat, separate out the first feature
-        and put it into its own list (this should be the 
+        and put it into its own list (this should be the
         quantity you want to predict)
 
         return targets and features as separate lists
 
-        (sklearn can generally handle both lists and numpy arrays as 
+        (sklearn can generally handle both lists and numpy arrays as
         input formats when training/predicting)
     """
 
@@ -124,5 +125,20 @@ def targetFeatureSplit( data ):
     return target, features
 
 
+def getFeaturesAsPandaDict(data, features_list):
+    """
+    data should be a list of features we're interested to have
+    """
+    data = featureFormat(data, ["poi"]+features_list )
+    poi, finance_features = targetFeatureSplit( data )
+
+    nparray = np.array(finance_features)
+
+    d={}
+    for i in range(0,len(features_list)):
+        d[features_list[i]] = nparray[:,i]
 
 
+    return pd.DataFrame(d)
+
+    
